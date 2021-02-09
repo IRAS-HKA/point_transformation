5b3001b5b31eb304c431d0d8c7c00452de1db404
Andreas Zachariae <zaan0001@hs-karlsruhe.de>
2021-02-09 19:03:58 +0100

    Initial commit

---
+++ ros/README.md
@@ -0,0 +1,6 @@
+# Point Transformation
+
+C++ based ROS Node for 2D / 3D Point Transformation
+
+# install
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/andreas/Code/iris_ws/build/poin_transformation_lib
---
+++ ros/src/point_transformation_node.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <point_transformation_lib/Transformation.h>
+
+int main(int argc, char **argv)
+{
+  (void)argc;
+  (void)argv;
+
+  printf("hello world point_transformation package\n");
+
+  auto t = Transformation();
+  return 0;
+}
